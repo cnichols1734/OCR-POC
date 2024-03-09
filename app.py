c6dd80597fcd640c7fd4a4cdc8e8bdc2edc89eef
cnichols1734 <chrisnichols17@gmail.com>
2024-03-09 01:29:19 -0600

    OCR POC

--- app.py
+++ app.py
@@ -2,38 +2,45 @@ import easyocr
 import json
 import re
 
+
 def extract_account_number(image_path):
     result = reader.readtext(image_path)
     extracted_text = ""
     for detection in result:
         text = detection[1]
         extracted_text += text + "\n"
-    account_number_match = re.search(r'(?:Account Number|Credit Card)[\s:]*([\d\s]+)', extracted_text, re.IGNORECASE)
-    return account_number_match.group(1).replace(" ", "").strip() if account_number_match else None
+    account_number_match = re.search(
+        r"(?:Account Number|Credit Card)[\s:]*([\d\s]+)", extracted_text, re.IGNORECASE
+    )
+    return (
+        account_number_match.group(1).replace(" ", "").strip()
+        if account_number_match
+        else None
+    )
+
 
 def process_files(file_paths):
     if isinstance(file_paths, str):
-        file_paths = [file_paths]  # Convert to list if only one file path is provided
+        file_paths = [file_paths]
     data = {}
     for file_path in file_paths:
         account_number = extract_account_number(file_path)
-        data[file_path] = {'account_number': account_number}
+        data[file_path] = {"account_number": account_number}
     return data
 
-# Create a reader object with English as the language option
-reader = easyocr.Reader(['en'])
 
-# Example usage with a single file
+reader = easyocr.Reader(["en"])
+
+# usage with a single file
 # image_paths = '/Users/cbn/Downloads/chase_cc_1.png'
 
-# Example usage with multiple files
+# usage with multiple files
 image_paths = [
-    '/Users/cbn/Downloads/chase_cc_1.png',
-    '/Users/cbn/Downloads/chase_6.png',
-    '/Users/cbn/Downloads/chase_5.png',
-    '/Users/cbn/Downloads/chase_4.jpeg',
-    '/Users/cbn/Downloads/chase_3.png',
-    # Add more file paths as needed
+    "/Users/cbn/Downloads/chase_cc_1.png",
+    "/Users/cbn/Downloads/chase statement 2.jpeg",
+    "/Users/cbn/Downloads/chase_5.png",
+    "/Users/cbn/Downloads/chase_4.jpeg",
+    "/Users/cbn/Downloads/chase_3.png",
 ]
 
 # Process the files and get the data
